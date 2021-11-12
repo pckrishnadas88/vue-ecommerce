d67fed4614176ebef82b06c365bb530c7112a30f
KrishnadasPC <pckrishnadas88@gmail.com>
2021-11-12 12:14:44 +0530

    init vue ecommerce.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home container">
+    <template v-for="(p) in products">   
+     
+        <div :key="p.id" class="col-md-4" style="margin-bottom:20px; float:left;">                
+            <div class="card" style="width: 20rem;">
+                <div class="card-body">
+                  <img :src="`http://localhost:8000/product-images/${ p.images[0].image }`" class="card-img-top" >
+
+                    <h5 class="card-title">{{ p.title.substring(0, 25) }}</h5>
+                    <h5 class="card-text text-primary">&#8377; {{ p.price }}</h5>
+                    <div class="badge badge-warning mt-3 mb-3" style="font-size:15px;"></div>
+                    <br>
+                    <a :href="`/products/${ p.id }`" class="btn btn-primary">View more details</a>
+                </div>
+            </div>                
+        </div>
+                
+    </template>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
+  data () {
+    return {
+      products: null
+    }
+  },
+  mounted () {
+    axios
+      .get("http://localhost:8000/api/products")
+      .then(response => {        
+        this.products = response.data.data;
+        console.log(response);   
+      })
+      .catch(function (error) {
+        console.log(error);
+    }) 
+  },
   components: {
-    HelloWorld,
+    //HelloWorld,
   },
 };
 </script>
